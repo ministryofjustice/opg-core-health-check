8500e880afe64f6b2d6ec77642058d8b24bd7210
Richard Saunders <richard@sdevsolutions.com>
2015-04-20 10:33:21 +0100

    SDV-1309 - service.config.php array structure change

--- config/service.config.php
+++ config/service.config.php
@@ -5,27 +5,25 @@ use HealthCheck\Service\EnvironmentVariablesChecker;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 return array(
-    'service_manager' => array(
-        'factories' => array(
-            'EnvironmentVariablesChecker' => function (ServiceLocatorInterface $serviceLocator) {
+    'factories' => array(
+        'EnvironmentVariablesChecker' => function (ServiceLocatorInterface $serviceLocator) {
 
-                /** @var array $config */
-                $config = $serviceLocator->get('Config');
+            /** @var array $config */
+            $config = $serviceLocator->get('Config');
 
-                /** @var FilePregMatcher $filePregMatcher */
-                $filePregMatcher = $serviceLocator->get('FilePregMatcher');
+            /** @var FilePregMatcher $filePregMatcher */
+            $filePregMatcher = $serviceLocator->get('FilePregMatcher');
 
-                return new EnvironmentVariablesChecker(
-                    $filePregMatcher,
-                    $config['health_check']['env_vars_markdown_uri']
-                );
-            },
-        ),
-        'services' => array(
-            'FilePregMatcher' => new FilePregMatcher(),
-        ),
-        'shared' => array(
-            'FilePregMatcher' => false,
-        ),
-    )
+            return new EnvironmentVariablesChecker(
+                $filePregMatcher,
+                $config['health_check']['env_vars_markdown_uri']
+            );
+        },
+    ),
+    'services' => array(
+        'FilePregMatcher' => new FilePregMatcher(),
+    ),
+    'shared' => array(
+        'FilePregMatcher' => false,
+    ),
 );
